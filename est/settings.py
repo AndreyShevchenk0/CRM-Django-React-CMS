b6ffe3787938868257956deb3442c1e4acd8c353
AndreyS <endry.sh.83.v@gmail.com>
2021-02-05 00:11:41 +0200

    6 comit установка реакта

--- est/settings.py
+++ est/settings.py
@@ -61,8 +61,8 @@ ROOT_URLCONF = 'est.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        #'DIRS': [os.path.join(BASE_DIR, 'build')],
+        #'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
